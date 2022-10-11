47d1036feca7cfd3d3aae1b807982ef0bc15216a
jiemojiemo <460706836@qq.com>
2022-10-11 13:50:02 +0800

    add 3_6_shaders_exercise3.cpp

---
+++ src/3_6_shaders_exercise3.cpp
@@ -0,0 +1,89 @@
+//
+// Created by user on 10/10/22.
+//
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+#include <cmath>
+#include <iostream>
+
+#include "learnopengl/shader_s.h"
+
+using namespace std;
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << "/path/to/vertex_shader_file /path/to/fragment_shader_file";
+        return -1;
+    }
+
+    // glfw initialize
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (!window) {
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // compile shaders
+    auto vertex_shader_path = argv[1];
+    auto fragment_shader_path = argv[2];
+    Shader our_shader(vertex_shader_path, fragment_shader_path);
+
+    // set up vao and vbo
+    float vertices[] = {
+        // 位置              // 颜色
+        0.5f, -0.5f, 0.0f,  // 右下
+        -0.5f, -0.5f, 0.0f, // 左下
+        0.0f, 0.5f, 0.0f,   // 顶部
+    };
+    GLuint vao{0};
+    GLuint vbo{0};
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(vao);
+    our_shader.use();
+    for (; !glfwWindowShouldClose(window);) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // render the triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
