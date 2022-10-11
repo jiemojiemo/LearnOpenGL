b52840281c45aa3888944e0a315500b03ba10453
jiemojiemo <460706836@qq.com>
2022-10-11 09:29:08 +0800

    add 3_5_shaders_exercise2.cpp

--- src/3_5_shaders_exercise2.cpp
+++ src/3_5_shaders_exercise2.cpp
@@ -72,6 +72,9 @@ int main(int argc, char *argv[]) {
 
     glBindVertexArray(vao);
     our_shader.use();
+    float offset = 0.5f;
+    our_shader.setFloat("xOffset", offset);
+
     for (; !glfwWindowShouldClose(window);) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
