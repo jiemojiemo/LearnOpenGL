c9c1b875b7a55d914f4d708f61f1ff9fc9c2ae99
jiemojiemo <460706836@qq.com>
2022-10-12 22:01:21 +0800

    add 4_6_textures_exercise4.cpp

---
+++ shaders/4.6.texture.fs
@@ -0,0 +1,16 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform float mixValue;
+
+// texture samplers
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(1.0-TexCoord.x, TexCoord.y)), mixValue);
+}
